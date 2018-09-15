acc7cc4b75f8cd43090d5f9a589224e1f46f4cbf
Calle <36276161+qalleh1@users.noreply.github.com>
2018-09-15 21:16:48 +0200

    simple ass scripts

---
+++ config.lua
@@ -0,0 +1,40 @@
+Config                            = {}
+
+Config.Teleporters = {
+	['EclipseTowers'] = {
+		['Job'] = 'police',
+		['Enter'] = { 
+			['x'] = -777.03137207031, 
+			['y'] = 319.640625, 
+			['z'] = 84.717643432617,
+			['Information'] = '[E] Go Up',
+		},
+		['Exit'] = {
+			['x'] = -781.83642578125, 
+			['y'] = 325.7190246582, 
+			['z'] = 175.85869567871, 
+			['Information'] = '[E] Go Down' 
+		}
+	},
+
+	['Cardealer'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 0, 
+			['y'] = 0, 
+			['z'] = 0,
+			['Information'] = '[E] Go Up' 
+		},
+
+		['Exit'] = {
+			['x'] = 0, 
+			['y'] = 0, 
+			['z'] = 0, 
+			['Information'] = '[E] Go Down' 
+		}
+	}
+
+	--Next here
+}
+
+
